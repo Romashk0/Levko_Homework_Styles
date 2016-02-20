c5bfdaf2ab6eb3bc2459d033d9bd89b591a7648e
RomanLevko <pifko22@gmail.com>
2016-02-20 10:43:19 +0200

    Initial commit

---
+++ app/src/main/java/com/levko/roma/levkohomeworkstyles/MainActivity.java
@@ -0,0 +1,13 @@
+package com.levko.roma.levkohomeworkstyles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
